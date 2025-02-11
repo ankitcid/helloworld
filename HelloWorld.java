78437f9d0aa70d17cd5f946339f4390c76bfa9c3
ankitcid <40690713+ankitcid@users.noreply.github.com>
2025-02-11 15:30:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    while (true) {
+      System.out.println("Hello world!");
+      try {
+        Thread.sleep(1000); // Pause for 1 second
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
